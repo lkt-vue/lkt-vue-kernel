e40eb9fe3b278ed2d5eb0b432078265dee4f6ab0
Antonio Ibáñez <am.ibanez.costa@gmail.com>
2026-01-03 18:25:09 +0100

    Update StepRenderType

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1516,7 +1516,8 @@ declare enum StepRenderType {
     RendersAndVisibleIfActive = 1,// Default
     AlwaysRendersVisibleIfActive = 2,
     AlwaysRendersVisibleAfterFirstActive = 3,
-    AlwaysRendersAlwaysVisible = 4
+    AlwaysRendersAlwaysVisible = 4,
+    RendersAfterFirstActiveVisibleIfActive = 5
 }
 
 interface StepProcessStepConfig {
--- src/enums/StepRenderType.ts
+++ src/enums/StepRenderType.ts
@@ -3,4 +3,5 @@ export enum StepRenderType {
     AlwaysRendersVisibleIfActive = 2,
     AlwaysRendersVisibleAfterFirstActive = 3,
     AlwaysRendersAlwaysVisible = 4,
+    RendersAfterFirstActiveVisibleIfActive = 5,
 }
